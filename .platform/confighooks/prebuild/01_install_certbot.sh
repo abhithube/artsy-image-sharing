6cc5d51a66e7f8917ea513d5d8f1ad502d1ea758
Abhi Thube <athube@abhithube.com>
2022-05-15 03:32:17 +0000

    Configured TLS encryption via certbot

---
+++ .platform/confighooks/prebuild/01_install_certbot.sh
@@ -0,0 +1,9 @@
+if [ ! -d "/etc/letsencrypt" ]
+then
+  sudo wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+  sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+  sudo yum-config-manager --enable epel*
+  sudo amazon-linux-extras install epel -y
+  
+  sudo yum install -y certbot python2-certbot-nginx
+fi
---
+++ .platform/hooks/postdeploy/01_get_tls_cert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo certbot -n --nginx -d artsy.abhithube.com --email athube@abhithube.com --agree-tos
